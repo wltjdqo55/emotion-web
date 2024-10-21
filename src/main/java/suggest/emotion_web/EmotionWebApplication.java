81d7a13697f12bfde4aa52aeb7db3984e1f6be80
지성배 <seongbae@eu-cast.com>
2024-10-21 15:03:34 +0900

    first commit

---
+++ src/main/java/suggest/emotion_web/EmotionWebApplication.java
@@ -0,0 +1,13 @@
+package suggest.emotion_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suggest/emotion_web/EmotionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package suggest.emotion_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

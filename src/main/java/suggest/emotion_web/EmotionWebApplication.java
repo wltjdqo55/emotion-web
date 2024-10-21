aa1d05445e8fb4d1b7655a3d94e81dc2b6aa187e
지성배 <seongbae@eu-cast.com>
2024-10-21 15:09:33 +0900

    241021
    초기 Properties, gradle 세팅

--- src/main/java/suggest/emotion_web/EmotionWebApplication.java
+++ src/main/java/suggest/emotion_web/EmotionWebApplication.java
@@ -2,8 +2,9 @@ package suggest.emotion_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EmotionWebApplication {
 
 	public static void main(String[] args) {

d177a944484d296204c2e2e9dde1c2b7fad1e784
지성배 <seongbae@eu-cast.com>
2024-10-22 17:04:09 +0900

    241022
    1. 로그인체크 구현
    2. querydsl API 구현

--- src/main/java/suggest/emotion_web/controller/AccountController.java
+++ src/main/java/suggest/emotion_web/controller/AccountController.java
@@ -1,5 +1,6 @@
 package suggest.emotion_web.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import suggest.emotion_web.model.dto.UserDTO;
 import suggest.emotion_web.model.vo.UserVO;
 import suggest.emotion_web.service.AccountService;
+import suggest.emotion_web.session.UserSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,5 +32,16 @@ public class AccountController {
   @ResponseBody
   public UserDTO userAdd (UserVO userVO) {
     return accountService.userAdd(userVO);
+  }
+
+  @PostMapping("/account/loginCheck")
+  @ResponseBody
+  public boolean loginCheck (UserVO userVO, HttpSession session) {
+    UserDTO userDTO = accountService.loginCheck(userVO);
+    if(userDTO != null) {
+      UserSession.setSession(userDTO, session, 3600);
+      return true;
+    }
+    return false;
   }
 }
--- src/main/java/suggest/emotion_web/service/AccountService.java
+++ src/main/java/suggest/emotion_web/service/AccountService.java
@@ -2,23 +2,35 @@ package suggest.emotion_web.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import suggest.emotion_web.model.dto.UserDTO;
 import suggest.emotion_web.model.entity.User;
 import suggest.emotion_web.model.vo.UserVO;
 import suggest.emotion_web.repository.jpa.AccountRepository;
+import suggest.emotion_web.repository.querydsl.AccountQueryDSL;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
   private final AccountRepository accountRepository;
+  private final AccountQueryDSL accountQueryDSL;
 
   public boolean idCheck (String userId) {
     return accountRepository.findOneByUserId(userId).isEmpty();
   }
 
+  @Transactional
   public UserDTO userAdd (UserVO userVO) {
     return new UserDTO(accountRepository.save(new User(userVO)));
+  }
+
+  public UserDTO loginCheck (UserVO userVO) {
+    User user = accountQueryDSL.loginCheck(userVO);
+    if(user==null) {
+      return null;
+    }
+    return new UserDTO(user);
   }
 
 }

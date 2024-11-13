265e6a76c961fa0691105b6ee41599c89105a72e
지성배 <seongbae@eu-cast.com>
2024-11-13 17:20:36 +0900

    241113
    1. 깃허브 운영서버 프로퍼티 설정 (테스트)

--- src/main/java/suggest/taste_the_weather/TasteTheWeatherWebApplication.java
+++ src/main/java/suggest/taste_the_weather/TasteTheWeatherWebApplication.java
@@ -1,5 +1,7 @@
 package suggest.taste_the_weather;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
